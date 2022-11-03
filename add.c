c014d8258afc7e148a118ef20470ecb15d3a05da
Souvik Ghosh <114401034+Souvik0216J@users.noreply.github.com>
2022-11-03 20:28:50 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a, b;
+	printf("Enter A Number:");
+	scanf("%d", &a);
+	printf("Enter A Number:");
+	scanf("%d", &b);
+	
+	printf("Total: %d", a+b);
+	return 0;
+}
